dff714afbac274320c61d1d7474082b443ec15eb
Mike Waterworth <mike.waterworth@epimorphics.com>
2018-08-08 23:57:30 +0100

    Add Vue moment for date filter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vueMoment from 'vue-moment'
+
+Vue.use(vueMoment)
+
+// var id = 2180673 // Number | ID of user to view
+//
+// var callback = function (error, data, response) {
+//   if (error) {
+//     console.error(error)
+//   } else {
+//     console.log('API called successfully. Returned data: ' + data)
+//   }
+// }
+// apiInstance.getContact(id, callback)
 
 Vue.config.productionTip = false
 
